2becb32652f529a65c44d306e85613e765923abc
Bilibili_MengChu <2133404320@qq.com>
2024-01-07 15:24:23 +0800

    Update

---
+++ VoiceChatPlugin/VoiceChatAdd.cs
@@ -0,0 +1,20 @@
+using BepInEx.Configuration;
+using NextShip.Api.Interfaces;
+using NextShip.Api.Services;
+
+namespace VoiceChatPlugin;
+
+public class VoiceChatAdd : INextAdd
+{
+    public void ServiceAdd(IServiceBuilder serviceBuilder)
+    {
+    }
+
+    public void DependentAdd(DependentService dependentService)
+    {
+    }
+
+    public void ConfigBind(ConfigFile config)
+    {
+    }
+}
---
+++ VoiceChatPlugin/VoiceChatPlugin.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using NextShip.Api;
+using NextShip.Api.Bases;
+using NextShip.Api.Interfaces;
+
+namespace VoiceChatPlugin;
+
+public class VoiceChatPlugin : ShipPlugin
+{
+    public Assembly RootAssembly { get; private set; }
+
+    public string RootPath = NextPaths.RootDirectory;
+    
+    public override void Load()
+    {
+        RootAssembly = Assembly.GetExecutingAssembly();
+    }
+
+    public override List<INextAdd> NextAdd()
+    {
+        return INextAdd.GetAdds(RootAssembly);
+    }
+}
